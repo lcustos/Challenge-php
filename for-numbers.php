2b0715a7d386d78937b78b9acaaa4bc9f701b15a
LCUSTOS <lucas.custos@ynov.com>
2022-09-28 09:38:57 +0200

    for-numbers.php

---
+++ for-numbers.php
@@ -0,0 +1,6 @@
+<?php for ($i=2;$i<100;$i++){$isPrime=1;
+    for($j=2;$j<=$i/2;$j++){
+        if ($i%$j ==0){$isPrime=0;break;}}
+    if ($isPrime==1){
+        echo "$i";
+        if ($i!==97){echo ", ";}}}?>
