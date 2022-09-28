388e5c7e6f7e87b6a2b18ab05e81f6baf9d67ffc
LCUSTOS <lucas.custos@ynov.com>
2022-09-28 09:09:03 +0200

    primitive-declarations.php

---
+++ primitive-declarations.php
@@ -0,0 +1,9 @@
+<?php
+$firstStr = "Bonjour";
+$firstEscapeStr = "This is escape str \n";
+$firstBool = true;
+$firstInteger=10;
+$firstFloat = 10.2;
+$firstNull = null;
+$firstArray =[2,null,'Hi',5.5,false];
+?>
