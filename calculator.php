9cae86523c96f63dab660c3e0b2c02430f045826
LCUSTOS <lucas.custos@ynov.com>
2022-09-30 10:46:00 +0200

    lift-v1.php

--- calculator.php
+++ calculator.php
@@ -2,5 +2,3 @@
 function calc($string): int{
     return eval("return $string;");
 }
-
-print calc('((5+3+(1+2)-9)+1) + (9+1)');
---
+++ lift-v1.php
@@ -0,0 +1,28 @@
+<?php
+function getFloor(int $currentFloor, ?int $requestFloor,array $buttonCalled): ?int{
+    if ($requestFloor !== null){
+        return $requestFloor;
+    }
+    if (!empty($buttonCalled)){
+        return nearestFloor($currentFloor, $buttonCalled);
+    }
+    return $currentFloor;
+
+}
+
+function nearestFloor(int $a, array $b): int
+{
+    $c = array_map(function ($x) use ($a) {
+        return abs($x - $a);
+    }, $b);
+    return $b[array_search(min($c), $c)];
+}
+
+function getDirection(int $currentFloor, ?int $requestFloor,array $buttonCalled){
+    if (getFloor($currentFloor,$requestFloor,$buttonCalled)>$currentFloor){
+        return 1;
+    }elseif (getFloor($currentFloor,$requestFloor,$buttonCalled)<$currentFloor){
+        return -1;
+    }
+    return 0;
+}
