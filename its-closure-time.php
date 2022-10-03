1c1ca1f4a878dabd5c72fd5ec96447e1b1949bda
LCUSTOS <lucas.custos@ynov.com>
2022-10-04 00:16:21 +0200

    its-closure-time.php

--- its-closure-time.php
+++ its-closure-time.php
@@ -1,25 +1,4 @@
 <?php
-function mul($x)
-{
-    return function ($y) use ($x) {
-        return $x * $y;
-    };
-}
-
-function add($x)
-{
-    return function ($y) use ($x) {
-        return $x + $y;
-    };
-}
-
-function sub($x)
-{
-    return function ($y) use ($x) {
-        return $x - $y;
-    };
-}
-
 function f():array{
     $array = array('+'=>function($a,$b){return $a+$b;},'-'=>function($a,$b){return $a-$b;},'*'=>function($a,$b){return $a*$b;});
     return $array;
