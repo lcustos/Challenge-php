a704a4d224ea8dae2f0703c129a377b173bdd755
LCUSTOS <lucas.custos@ynov.com>
2022-10-05 10:50:25 +0200

    geometry-master.php

---
+++ geometry-master.php
@@ -0,0 +1,58 @@
+<?php
+
+abstract class AbstractGeometry{
+    abstract public function area();
+    abstract public function perimeter();
+}
+
+class Rectangle extends AbstractGeometry{
+    public $width;
+    public $height;
+    public function __construct($width, $height) {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function area() {
+        return $this->width * $this->height ;
+    }
+    public function perimeter() {
+        return ($this->width * $this->height)*2;
+    }
+}
+
+class Square extends AbstractGeometry{
+    public $width;
+
+    public function __construct($width) {
+        $this->width = $width;
+    }
+
+    public function area() {
+        return $this->width **2;
+    }
+    public function perimeter() {
+        return $this->width * 4;
+    }
+
+}
+
+class Triangle extends AbstractGeometry{
+    public function __construct($a,$b,$c) {
+    $this->a = $a;
+    $this->b = $b;
+    $this->c = $c;
+    }
+    public function perimeter() {
+        return $this->a + $this->b + $this->c;
+    }
+
+    public function area(): float
+    {
+        $P= $this->a + $this->b + $this->c;
+        $p=$P/2;
+        return sqrt($p*($p-$this->a)*($p-$this->b)*($p-$this->c));
+
+    }
+
+}
