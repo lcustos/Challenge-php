2a383433fd8927905948465e71a3c492bbfac130
LCUSTOS <lucas.custos@ynov.com>
2022-10-05 11:32:50 +0200

    map-this-callable.php

--- map-this-callable.php
+++ map-this-callable.php
@@ -1,8 +1,11 @@
 <?php
 function myArrayMap(callable $callable, array $array, $initial=null):array{
-    $result = $array;
+    $result = $initial;
     foreach ($array as $value) {
         $result[] = $callable($value);
+    }
+    if ($callable === null) {
+        return $array;
     }
     return $result;
 }
